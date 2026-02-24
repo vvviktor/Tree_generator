edf108dbe49b44ae077398ec9bc743a64e5e08c7
Viktor Kustov <kustovvictor@gmail.com>
2026-02-24 22:48:32 +0300

    rendering/ apply padding to x coord

--- tree_renderer/tree_renderer.cpp
+++ tree_renderer/tree_renderer.cpp
@@ -33,14 +33,15 @@ void TreeRenderer::DFS_Omni(svg::Document& doc,
     for (const auto& ch : root->ch) {
         DFS_Omni(doc, ch, root);
     }
-    svg::Point this_node(root->u.x, render_settings_.max_y - root->u.y +
-                                        render_settings_.padding);
+    svg::Point this_node(
+        root->u.x + render_settings_.padding,
+        render_settings_.max_y - root->u.y + render_settings_.padding);
     svg::Circle node;
     node.SetCenter(this_node).SetRadius(render_settings_.node_radius);
     if (parent) {
-        svg::Point parent_node(parent->u.x, render_settings_.max_y -
-                                                parent->u.y +
-                                                render_settings_.padding);
+        svg::Point parent_node(parent->u.x + render_settings_.padding,
+                               render_settings_.max_y - parent->u.y +
+                                   render_settings_.padding);
         svg::Polyline edge;
         edge.AddPoint(this_node)
             .AddPoint(parent_node)
@@ -64,12 +65,13 @@ void TreeRenderer::DFS_Bin_Render(
     }
     DFS_Bin_Render(doc, root->left, root);
     DFS_Bin_Render(doc, root->right, root);
-    svg::Point this_node(root->u.x, render_settings_.max_y - root->u.y +
-                                        render_settings_.padding);
+    svg::Point this_node(
+        root->u.x + render_settings_.padding,
+        render_settings_.max_y - root->u.y + render_settings_.padding);
     if (parent) {
-        svg::Point parent_node(parent->u.x, render_settings_.max_y -
-                                                parent->u.y +
-                                                render_settings_.padding);
+        svg::Point parent_node(parent->u.x + render_settings_.padding,
+                               render_settings_.max_y - parent->u.y +
+                                   render_settings_.padding);
         svg::Polyline edge;
         edge.AddPoint(this_node)
             .AddPoint(parent_node)
@@ -95,12 +97,13 @@ void TreeRenderer::DFS_Tern_Render(
     DFS_Tern_Render(doc, root->left, root);
     DFS_Tern_Render(doc, root->mid, root);
     DFS_Tern_Render(doc, root->right, root);
-    svg::Point this_node(root->u.x, render_settings_.max_y - root->u.y +
-                                        render_settings_.padding);
+    svg::Point this_node(
+        root->u.x + render_settings_.padding,
+        render_settings_.max_y - root->u.y + render_settings_.padding);
     if (parent) {
-        svg::Point parent_node(parent->u.x, render_settings_.max_y -
-                                                parent->u.y +
-                                                render_settings_.padding);
+        svg::Point parent_node(parent->u.x + render_settings_.padding,
+                               render_settings_.max_y - parent->u.y +
+                                   render_settings_.padding);
         svg::Polyline edge;
         edge.AddPoint(this_node)
             .AddPoint(parent_node)
